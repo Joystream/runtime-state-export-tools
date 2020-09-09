c88ac7007b76a019628dcb56b00965907b564b9b
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2020-09-09 08:57:30 +0300

    prettier formatting

--- src/api.ts
+++ src/api.ts
@@ -1,21 +1,21 @@
 // @ts-check
 
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import { registerJoystreamTypes } from '@joystream/types';
+import { ApiPromise, WsProvider } from '@polkadot/api'
+import { registerJoystreamTypes } from '@joystream/types'
 
-export default async function create_api () {
+export default async function create_api() {
   // Get URL to websocket endpoint from environment or connect to local node by default
   const WS_URL = process.env['WS_URL'] || 'ws://127.0.0.1:9944'
 
   // Initialise the provider
-  const provider = new WsProvider(WS_URL);
+  const provider = new WsProvider(WS_URL)
 
   // register types before creating the api
-  registerJoystreamTypes();
+  registerJoystreamTypes()
 
   // Create the API and wait until ready
-  let api = await ApiPromise.create({ provider });
-  await api.isReady;
+  let api = await ApiPromise.create({ provider })
+  await api.isReady
 
-  return api;
+  return api
 }
