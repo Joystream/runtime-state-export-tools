4edb84c24855549cc3de9a70e2a32b21a087fb49
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2021-02-16 09:54:53 +0200

    export members

--- src/api.ts
+++ src/api.ts
@@ -1,28 +1,17 @@
 // @ts-check
 
 import { ApiPromise, WsProvider } from '@polkadot/api'
-import { registerJoystreamTypes } from '@joystream/types'
-import { getTypeRegistry } from '@polkadot/types'
-import { ActiveStake } from './overrideTypes'
+import { types } from '@joystream/types'
 
-export default async function create_api() {
+export default async function createApi() {
   // Get URL to websocket endpoint from environment or connect to local node by default
-  const WS_URL = process.env['WS_URL'] || 'ws://127.0.0.1:9944'
+  const WS_URL = process.env.WS_URL || 'ws://127.0.0.1:9944'
 
   // Initialise the provider
   const provider = new WsProvider(WS_URL)
 
-  // register types before creating the api
-  registerJoystreamTypes()
-
-  // override incorrectly registerd type: ActiveStake
-  const typeRegistry = getTypeRegistry()
-  typeRegistry.register({
-    ActiveStake,
-  })
-
   // Create the API and wait until ready
-  const api = await ApiPromise.create({ provider })
+  const api = await ApiPromise.create({ provider, types })
   await api.isReady
 
   return api
