e2b228ec92aa1765cb287de6facb5da55779fba1
ondratra <ondratra@gmail.com>
2021-05-10 11:51:29 +0200

    support for Sumer + workers export III

--- src/export_workers.ts
+++ src/export_workers.ts
@@ -3,33 +3,45 @@ import { Worker, WorkerId } from '@joystream/types/working-group'
 import { StorageKey } from '@polkadot/types';
 import { AnyTuple } from '@polkadot/types/types';
 
+// run export
 main()
 
+// export flow
 async function main() {
+  // prepare api connection
   const api = await createApi()
 
+  // get results for all relevant groups
   const result = {
     storage: await getAllWorkers(api.query.storageWorkingGroup),
     gateway: await getAllWorkers(api.query.gatewayWorkingGroup),
   }
+
+  // output results
   console.log(JSON.stringify(result))
 
+  // disconnect api
   api.disconnect()
 }
 
+// partial interface for working with working group
 interface WorkingGroup {
-  nextWorkerId: () => Promise<WorkerId>
   workerById: {
     entries: () => Promise<[StorageKey<AnyTuple>, Worker][]> // working group entries
   }
 }
 
+// retrieves all active workers in working group
 async function getAllWorkers(workingGroupApi: WorkingGroup): Promise<any[]> {
+  // get working group entries
   const entries = await workingGroupApi.workerById.entries()
-  const workers = [] as any[]
-  for (const [storageKey, rawWorker] of entries) {
-    const workerId = (storageKey.args[0] as any).toNumber()
 
+  const workers = []
+  for (const [storageKey, rawWorker] of entries) {
+    // prepare workerId
+    const workerId = (storageKey.args[0] as WorkerId).toNumber()
+
+    // save record
     workers.push({
       id: workerId,
       member_id: rawWorker.member_id,
