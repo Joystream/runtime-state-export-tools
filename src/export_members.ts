4cb4ff27d74ea67ee18253518933f4cef4266f86
Joystream Stats <dev@joystreamstats.live>
2022-02-15 17:38:35 +0100

    Revert "olympia: add member.name" (no longer needed)
    This reverts commit 5e4bf0d9c777f92096038c2300786ebff7e135f3.

--- src/export_members.ts
+++ src/export_members.ts
@@ -42,7 +42,6 @@ async function getAllMembers(
       root_account: p.root_account,
       controller_account: p.controller_account,
       handle: p.handle,
-      name: ``,
       avatar_uri: p.avatar_uri,
       about: p.about,
       registered_at_time: p.registered_at_time.toNumber(),
